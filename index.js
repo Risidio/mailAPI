8b31a1a4fe4fef6f4aa8ca23cbf81630c5c45339
Bara Konateh <bkonateh1@gmail.com>
2022-03-30 12:33:09 +0100

    Update
    Updated for risidio

--- index.js
+++ index.js
@@ -12,58 +12,56 @@ app.use(cors())
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     pool: true,
-    host: "smtp-mail.outlook.com", // hostname
+    host: "uk53.siteground.eu", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
+    port: 465, // port for secure SMTP
     tls: {
-       ciphers:'SSLv3'
+      ciphers:'SSLv3'
     },
     auth: {
-        user: process.env.A1,
-        pass: process.env.A2
+      user: process.env.A1,
+      pass: process.env.A2
     }
 })
 
 transporter.verify((error) => {
-  if (error) {console.log(error);} 
+  if (error) {console.log(error);}
   else {console.log("Ready to Send");}
 });
 
-  app.post("/contact", (req,res, next)=>{
+  app.post("/contact", (req,res)=>{
     const mailOptions = {
-        from: process.env.A1, // sender address (who sends)
-        to: process.env.A3, // list of receivers (who receives)
-        subject: `${req.body.name} Sent you a message from the Portfolio Contact Form!`, // Subject line
-        text: `${req.body.message}`, // plaintext body.contact
-        html: `<h1>${req.body.name} sent you a message from your website.</h1>
-                <br/>
-                <h3>Their email is: ${req.body.email}</h3>
-                <br/>
-                <h4>Message below:</h4>
-                <p>${req.body.message}</p>` // html body
-      };
+      from: process.env.A1, // sender address (who sends)
+      to: process.env.A3, // list of receivers (who receives)
+      subject: `${req.body.name} sent you a message from the Indige Contact Form!`, // Subject line
+      text: `${req.body.message}`, // plaintext body.contact
+      html: `<div style="margin: 20px; text-align: left; border: solid 1px grey; border-radius: 5px; padding: 20px;">
+              <h3 style="text-align: center;">You recieved a new message from your Indige Form </h3>
+              <hr style="margin: 20px; color: grey;"/>
+              <br/>
+              <h3>Name:</h3>
+              <p>${req.body.name}</p>
+              <h3>Their email is:</h3>
+              <p>${req.body.email}</p>
+              <h3>Subject:</h3>
+              <p>${req.body.subject}</p>
+              <h3>Message below:</h3>
+              <p>${req.body.message}</p>
+            </div>` // html body
+    };
       console.log(req.body)
       res.json({status: 'Request Successful!'})
-    
+
     // send mail with defined transport object
     transporter.sendMail(mailOptions, function(error, info){
         if(error){
           res.json({status: 'Request Failed'})
-             console.log(error);
+          console.log(error);
         } else {
-            console.log('Message sent: ' + info.response);
-            res.json({ status: "Email sent" });
+          console.log('Message sent: ' + info.response);
+          res.json({ status: "Email sent" });
         }
     });
-    
-    // transporter.verify((error) => {
-    //     if (error) {
-    //       console.log(error);
-    //     } else {
-    //       console.log("Ready to Send");
-    //     }
-    //   });
-    // next();
   })
 
 const PORT = process.env.PORT || 4000
